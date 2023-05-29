f6a1b4ec6e53baa34345c0b58c1493cb4ed69da6
theng so <rae6352500277@gmail.com>
2023-05-29 18:59:35 +0700

    g

---
+++ build/urpro/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/thengso/tutorial_ws/src/urpro
+# Build directory: /home/thengso/tutorial_ws/build/urpro
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
